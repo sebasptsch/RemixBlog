3ccd646870e46b5be14d9a5367d44a2da4fa1492
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-09-04 05:36:39 +0000

    session

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,6 +1,7 @@
 // app/services/session.server.ts
 import { createCookieSessionStorage } from "@remix-run/node";
-
+import { createRedisSessionStorage } from "remix-redis-session";
+import { config } from "./config.server";
 // export the whole sessionStorage object
 export let sessionStorage = createCookieSessionStorage({
   cookie: {
@@ -8,7 +9,7 @@ export let sessionStorage = createCookieSessionStorage({
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
-    secrets: ["s3cr3t"], // replace this with an actual secret
+    secrets: [config.getOrThrow("SESSION_SECRET")], // replace this with an actual secret
     secure: process.env.NODE_ENV === "production", // enable this in prod only
   },
 });
